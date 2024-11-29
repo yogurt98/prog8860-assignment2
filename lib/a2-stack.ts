6936c55a5f96a9c43325f73aecd57c0b627df9ea
yogurt98 <yogurt98@HeavensFeel>
2024-11-29 02:45:56 -0500

    Initial commit for AWS CDK project

--- lib/a2-stack.ts
+++ lib/a2-stack.ts
@@ -1,16 +1,50 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class A2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 bucket
+    const myBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Remove bucket when stack is deleted (for dev/test environments only)
+    });
 
-    // example resource
+    // Create a Lambda function
+    const myLambda = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X, // Updated to use supported Node.js runtime
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('Lambda invoked!');
+          return { statusCode: 200, body: 'Hello, World!' };
+        }
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName, // Environment variable for bucket name
+      },
+    });
+
+    // Grant the Lambda function permissions to access the S3 bucket
+    myBucket.grantReadWrite(myLambda);
+
+    // Create a DynamoDB table
+    const myTable = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'MyTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Remove table when stack is deleted (for dev/test environments only)
+    });
+
+    // Grant the Lambda function permissions to access the DynamoDB table
+    myTable.grantFullAccess(myLambda);
+
+    // Example resource comment (if needed)
     // const queue = new sqs.Queue(this, 'A2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    //   visibilityTimeout: cdk.Duration.seconds(300),
     // });
   }
 }
